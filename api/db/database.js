6806adef3f9b1da8cf0b60610f66f6afa880ed44
Sabin Dahal <153823999+sabindahal2004@users.noreply.github.com>
2024-02-22 09:44:31 +0545

    Delete api directory

--- api/db/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
-const db_url = process.env.MONGO_URL;
-mongoose.connect(db_url)
-.then(()=>console.log("Database connection success!!"))
-.catch(err => console.log(err));
